40d89e6113f402d86414112b550be87b605c145d
kissy89 <stephanie.senoner@gmail.com>
2018-02-13 15:46:23 +0100

    testing new user and places

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,24 @@ const User = require('../models/user');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  User.find({}, (err, users) => {
+  // User.find({}, (err, users) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   const data = {
+  //     users
+  //   };
+  // });
+
+  User.find({}).populate('places').exec((err, users) => {
     if (err) {
       return next(err);
     }
+
     const data = {
       users
     };
+
     res.render('index', data);
   });
 });
