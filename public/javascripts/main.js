c5d91a264cfa430335d6718bca4f16d18c61c038
kissy89 <stephanie.senoner@gmail.com>
2018-02-15 20:39:51 +0100

    Autocomplete API added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,11 +2,9 @@
 function main () {
   function selectedChange (event) {
     const nameInputElement = document.getElementById('name');
-    const descriptionInputElement = document.querySelector('#description');
     const latitudeInputElement = document.querySelector('#latitude');
     const longitudeInputElement = document.querySelector('#longitude');
 
-    console.log(event.target.value);
     const id = event.target.value;
 
     // get data from the api
@@ -15,7 +13,6 @@ function main () {
         const place = response.data.place;
 
         nameInputElement.value = place.name;
-        descriptionInputElement.value = place.description;
         latitudeInputElement.value = place.location.coordinates[0];
         longitudeInputElement.value = place.location.coordinates[1];
       })
@@ -24,8 +21,29 @@ function main () {
       });
   }
 
-  const choice = document.getElementById('choice');
-  choice.addEventListener('change', selectedChange);
+  function initializeAutocomplete () {
+    const input = document.getElementById('locality');
+
+    const autocomplete = new google.maps.places.Autocomplete(input);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+      let place = autocomplete.getPlace();
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+      const name = place.name;
+
+      document.getElementById('latitude').value = lat;
+      document.getElementById('longitude').value = lng;
+      document.getElementById('name').value = name;
+    });
+  }
+
+  // const choice = document.getElementById('choice');
+  // choice.addEventListener('change', selectedChange);
+  const localityElement = document.querySelector('#locality');
+  localityElement.addEventListener('focus', initializeAutocomplete);
 }
 
-window.onload = main;
+main();
+
+// window.onload = main;
