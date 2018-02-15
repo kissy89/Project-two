7e7f45f2e69e35bf0ffd4b27623fe72f86602da4
kissy89 <stephanie.senoner@gmail.com>
2018-02-15 13:10:43 +0100

    center geolocation added

--- public/javascripts/usermap.js
+++ public/javascripts/usermap.js
@@ -19,7 +19,7 @@ function main () {
         const map = new google.maps.Map(
           document.getElementById('map'),
           {
-            zoom: 10,
+            zoom: 15,
             center: center
           }
         );
